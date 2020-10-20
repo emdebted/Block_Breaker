ea05d99ba9fbed229800eb76e1fe15f7ebdfe2a1
Emily Parker <emdebted123@gmail.com>
2020-10-20 06:59:21 -0500

    Expanded game audio

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,13 +7,18 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds; 
 
     Vector2 paddleToBallVector;
-    public bool hasStarted = false; 
+    public bool hasStarted = false;
+
+    // Cached component references
+    AudioSource myAudioSource;
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -42,7 +47,8 @@ public class Ball : MonoBehaviour
     {
         if (hasStarted)
         {
-            GetComponent<AudioSource>().Play();
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
         }
     }
 }
