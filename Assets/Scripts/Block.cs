ba122fd77578f2a969968385bd9dc41a32e4df96
Emily Parker <emdebted123@gmail.com>
2020-11-11 02:13:12 -0600

    Added VFX, added unbreakable blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+
     Level level;
-    void Start()
+
+    private void Start()
     {
         level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
     }
-    void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (tag == "Breakable")
+        {
+            DestroyBlock();
+        }
+    }
+
+    private void DestroyBlock()
     {
         FindObjectOfType<GameStatus>().AddToScore();
         level.BlockDestroyed();
         AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position);
+        TriggerSparklesVFX();
         Destroy(gameObject);
+    }
+
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
     }
 }
